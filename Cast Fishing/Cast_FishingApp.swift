72edebc92bd225115bc3d80328d1249e49df766c
theojandhyala@icloud.com <theojandhyala@icloud.com>
2026-06-12 22:15:28 +0100

    Initial Commit

---
+++ Cast Fishing/Cast_FishingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cast_FishingApp.swift
+//  Cast Fishing
+//
+//  Created by Theo Jandhyala on 12/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Cast_FishingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cast Fishing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cast Fishing
+//
+//  Created by Theo Jandhyala on 12/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
